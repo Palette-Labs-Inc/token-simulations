76bd7971dbf8406bab8f4543f6d75e8521c78715
Kiran Karra <kiran.karra@gmail.com>
2024-06-10 06:49:15 -0700

    simulation updates

--- nosh/agent.py
+++ nosh/agent.py
@@ -8,6 +8,7 @@ class Agent:
             num_tokens=0, 
             transact_probability=0.5,
             reputation_process_kwargs=None,
+            transaction_draw_kwargs=None,
             dt=1
     ):
         self.agent_id = agent_id
@@ -16,6 +17,7 @@ class Agent:
 
         self.rng = rng
 
+        # brownian motion --> normal distribution
         if reputation_process_kwargs is None:
             reputation_process_kwargs = {
                 'mu': 1,
@@ -26,9 +28,21 @@ class Agent:
             assert 'mu' in reputation_process_kwargs
             assert 'sigma' in reputation_process_kwargs
             assert 'start_value' in reputation_process_kwargs
+        
+        ## assume Gamma distribution for transaction values, since we want strict positivity
+        if transaction_draw_kwargs is None:
+            transaction_draw_kwargs = {
+                'shape': 2,
+                'scale': 2,
+            }
+        else:
+
+            assert 'shape' in transaction_draw_kwargs
+            assert 'scale' in transaction_draw_kwargs
         # seed reputation process
         self.reputation_vector = [reputation_process_kwargs['start_value']]
         self.reputation_process_kwargs = reputation_process_kwargs
+        self.transaction_draw_kwargs = transaction_draw_kwargs
         self.dt = dt
 
     def will_transact(self):
@@ -47,5 +61,7 @@ class Agent:
         return self.reputation_vector[-1]
     
     def compute_transaction_value(self, counterparty_node):
-        # Example: The transaction value is based on some logic
-        return 10  # Placeholder value
+        return self.rng.gamma(
+            shape=self.transaction_draw_kwargs['shape'],
+            scale=self.transaction_draw_kwargs['scale']
+        )
